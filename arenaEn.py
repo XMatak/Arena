1fa6bb18f0fd02f951d73dc62540b9561f249b03
XMatak <101801527+XMatak@users.noreply.github.com>
2022-03-30 18:19:46 +0200

    Update arenaEn.py

--- arenaEn.py
+++ arenaEn.py
@@ -1,34 +1,10 @@
 import time
 import random
-from player_mod import player
-
-
-hero = None
-
-class colors: # barvy / colors
-    PINK = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    ENDC = '\033[0m'
-    STORY = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 def decorateText(text):
-    time.sleep(1)
     #print(f"{color}{text}{colors.ENDC}")
     # nefunguje v idlu :(
-    print(text)
-    
-
-def stats(): # proste statistiky
-#    decorateText(You sit down and think about it all. \nYou've killed {playerKills} creatures... and you died {playerDeaths} times.")
-#    decorateText(Worth it?")
-#    decorateText("You sit down and think about it all. \nYou've killed creatures..." + playerKills + "and you died" + playerDeaths + "times." )
-    decorateText("Worth it?")
-    decorateText("Maybe...")
+    input(text)
           
 def playerDeath(): # nahodna zprava o smrti
     deathMessages = ["A sword impales you. You died so early?", "Announcer: And a fatal hit! The human is defeated!", "An arrow pierces your skull, you wonder if it was an enemy, or a bystander.", "Weakling! he taunts you. No match for me!"]
@@ -51,20 +27,19 @@ def startStory():   # prvni smrt? spis dialog a jestli je hrac ready
 
 def selectHeroName():
     playerName = input("Just wondering, what is your name? ")
-    time.sleep(1)
     print(playerName + "... cool!")
-    time.sleep(1)
     return playerName
 
 def selectHeroType():
     global hero
-    heroName = selectHeroName()
     isNotHeroChosen = True
     while isNotHeroChosen:
         heroType = input("Who are you exactly? [mage / barb] ")
-        if str(heroType) == "mage" or str(heroType) == "barb":
-            hero = player(heroName, heroType)
+        if str(heroType) == "barb":
             isNotHeroChosen = False
+        elif str(heroType) == "mage":
+            decorateText("Mages are not allowed in this arena.")
+            decorateText("You get a feeling that they have not been coded yet.")
         else:
             decorateText("He asks you again, unsatisfied with your answer.")
             
@@ -74,7 +49,7 @@ def selectHeroType():
     decorateText("There are no rules.")
 
 def getRandomEnemyName():
-    enemyNames = ["Josef", "Gragas", "Blitzcrank", "Ash", "Doom", "Gangplank", "Halo", "Savathun", "Bowser", "Void", "'merica", "Wise Old Man", "Aatrox", "Tryndamere"]
+    enemyNames = ["Josef", "Gragas", "Blitzcrank", "Ash", "Doom", "Gangplank", "Halofungaming", "Savathun", "Bowser", "Void", "'merica", "Wise Old Man", "Aatrox", "Tryndamere"]
     return enemyNames[random.randint(0,13)]
 
 def getRandomEnemyHP():
@@ -83,75 +58,75 @@ def getRandomEnemyHP():
 def battle(heroName, heroType, enemyName, getEnemyHP): # bez objektoveho programovani, trochu prasarna
     isEnemyDead = False
     isPlayerDead = False
-    heroHP = 10
+    heroHP = random.randint(100,150)
     enemyHP = getEnemyHP
     while isEnemyDead == False and isPlayerDead == False:
-        if heroType == "barb":
-            decorateText("Choose your move:")
-            playerMove = input("Attack / Super / Heal / Check [A/S/H/C] ")
-            if playerMove == "A":
-                attackText = ["You bonk him. The ding could be heard anywhere.", "You slap him. Ow.", "You dropkick him. He flew a bit.", "Spin to win is the strat, amiright?", "You swing your fists and manage to hit!"]
-                decorateText(attackText[random.randint(0, 4)])
-                enemyHP -= random.randint(10, 20)
-            elif playerMove == "S":
-                if heroHP >= 20:
-                    decorateText("You concentrate.")
-                    decorateText("... you don't feel angry enough.")
-                else:
-                    decorateText("The wrath of your ancestors calls upon you.")
-                    decorateText("You feel the fury in your veins.")
-                    decorateText("Your enemy is scared about what is going to happen.")
-                    decorateText("He should be.")
-                    decorateText("You start channeling your power:")
-                    decorateText("The audience looks at you in anticipation.")
-                    decorateText("You go full anime mode.")
-                    decorateText("HIT!")
-                    print("HIT!")
-                    print("HIT!")
-                    print("HIT!")
-                    print("HIT!")
-                    print("HIT!")
-                    print("HIT!")
-                    print("HIT!")
-                    print("YOU ARE UNSTOPPABLE!")
-                    print("HIT!")
-                    print("HIT!")
-                    print("HIT!")
-                    print("HIt")
-                    print("Hit")
-                    print("hit")
-                    decorateText("h i t")
-                    decorateText("Too tired to continue.")
-                    decorateText("Poor guy you think.")
-                    decorateText("He looks really. really. REALLY not good.")
-                    enemyHP -= random.randint(60, 100)
-            elif playerMove == "H":
-                decorateText("You concentrate on mending your wounds.")
-                decorateText("You feel a little better.")
-                heroHP += random.randint(20, 30)
-            elif playerMove == "C":
-                decorateText("What was his name?")
-                decorateText(enemyName)
-                decorateText("something like that...")
-                decorateText("You thoroughly look at your opponent.")
-                if enemyHP > 100:
-                    decorateText("He looks scratchless.")
-                elif enemyHP > 80:
-                    decorateText("He looks fine.")
-                elif enemyHP > 60:
-                    decorateText("He looks wounded a bit.")
-                elif enemyHP > 40:
-                    decorateText("He looks beaten up.")
-                elif enemyHP > 20:
-                    decorateText("Yeah, he doesn't look good.")
-                elif enemyHP > 0:
-                    decorateText("He is a walking corpse.")
-                else:
-                    decorateText("He is dead.")
-                    decorateText("You've been beating a dead horse.")
-                    decorateText("You won.")
-                    decorateText("... but at what costs?")
-                    isEnemyDead = True
+        print("Choose your move:")
+        playerMove = input("Attack / Super / Heal / Check [A/S/H/C] ")
+        if playerMove == "A":
+            attackText = ["You bonk him. The ding could be heard anywhere.", "You slap him. Ow.", "You dropkick him. He flew a bit.", "Spin to win is the strat, amiright?", "You swing your fists and manage to hit!"]
+            decorateText(attackText[random.randint(0, 4)])
+            enemyHP -= random.randint(10, 20)
+        elif playerMove == "S":
+            if heroHP >= 20:
+                decorateText("You concentrate.")
+                decorateText("... you don't feel angry enough.")
+            else:
+                decorateText("The wrath of your ancestors calls upon you.")
+                decorateText("You feel the fury in your veins.")
+                decorateText("Your enemy is scared about what is going to happen.")
+                decorateText("He should be.")
+                decorateText("You start channeling your power:")
+                decorateText("The audience looks at you in anticipation.")
+                decorateText("You go full anime mode.")
+                decorateText("HIT!")
+                print("HIT!")
+                print("HIT!")
+                print("HIT!")
+                print("HIT!")
+                print("HIT!")
+                print("HIT!")
+                print("HIT!")
+                print("YOU ARE UNSTOPPABLE!")
+                print("HIT!")
+                print("HIT!")
+                print("HIT!")
+                print("HIt")
+                print("Hit")
+                print("hit")
+                decorateText("h i t")
+                decorateText("Too tired to continue.")
+                decorateText("Poor guy you think.")
+                decorateText("He looks really. really. REALLY not good.")
+                enemyHP -= random.randint(60, 100)
+        elif playerMove == "H":
+            decorateText("You concentrate on mending your wounds.")
+            decorateText("You feel a little better.")
+            heroHP += random.randint(20, 30)
+        elif playerMove == "C":
+            decorateText("What was his name?")
+            decorateText(enemyName)
+            decorateText("something like that...")
+            decorateText("You thoroughly look at your opponent.")
+            if enemyHP > 100:
+                decorateText("He looks scratchless.")
+            elif enemyHP > 80:
+                decorateText("He looks fine.")
+            elif enemyHP > 60:
+                decorateText("He looks wounded a bit.")
+            elif enemyHP > 40:
+                decorateText("He looks beaten up.")
+            elif enemyHP > 20:
+                decorateText("Yeah, he doesn't look good.")
+            elif enemyHP > 0:
+                decorateText("He is a walking corpse.")
+            else:
+                decorateText("He is dead.")
+                decorateText("You've been beating a dead horse.")
+                decorateText("You won.")
+                decorateText("... but at what costs?")
+                decorateText("A new challenger appears.")
+                isEnemyDead = True
                             
         enemyMove = random.randint(1,4)
         if enemyMove == 1: # attack
@@ -202,15 +177,33 @@ def battle(heroName, heroType, enemyName, getEnemyHP): # bez objektoveho program
                 decorateText("You've been given a chance until he confirms your kill.")
                 decorateText("And yet. you failed.")
                 decorateText("You lost.")
+                decorateText("As the world fades around you a new face comes to the arena.")
+                decorateText("He looks like...")
+                decorateText("You!")
+                decorateText("It starts to make sense now.")
+                decorateText("Anyways, you're dead.")
+                decorateText("And the guy is going to fight next.")
+                decorateText("Or is it you?")
+                decorateText("I don't know, I am lost.")
+
                 isPlayerDead = True
                 
                 
                     
                 
-
+playerName = None
+playerType = None
 def mainLoop():
-    #startStory()
-    #battle(selectHeroName(), selectHeroType(), getRandomEnemyName(), getRandomEnemyHP())
-    battle("pepa", "barb", "dummy", 150)
+    playTheGame = True
+    startStory()
+    playerName = selectHeroName()
+    playerType = selectHeroType()
+    battle(playerName, playerType, getRandomEnemyName(), getRandomEnemyHP())
+    while playTheGame:
+        if input("Do you want to play more? [any / n] ") == "n":
+            decorateText("See you next time.")
+        else:
+            decorateText("Very well then.")
+            battle(playerName, playerType, getRandomEnemyName(), getRandomEnemyHP())
 
 mainLoop()
